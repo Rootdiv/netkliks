0a98d2322a1803cd8ee1aa967e9b7cd109c81c23
Rootdiv <rootdiv9152@gmail.com>
2024-09-29 15:50:04 +0300

    feat: add film category and description as array

--- src/app/models/film.model.ts
+++ src/app/models/film.model.ts
@@ -2,9 +2,9 @@ export interface IFilm {
   id: number;
   title: string;
   original: string;
-  category: string;
+  category: string[];
   rating: number;
   image: string;
   link: string;
-  description: string;
+  description: string[];
 }
